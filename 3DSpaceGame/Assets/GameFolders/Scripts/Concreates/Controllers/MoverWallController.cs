7a95e0d934c92e09e13af4b0b77d0edfe8ff29b9
BasarEkinci <basarekinci02@outlook.com>
2023-03-03 17:04:47 +0300

    Mover Wall
    Mover Wall and MoverWall script completed.

--- 3DSpaceGame/Assets/GameFolders/Scripts/Concreates/Controllers/MoverWallController.cs
+++ 3DSpaceGame/Assets/GameFolders/Scripts/Concreates/Controllers/MoverWallController.cs
@@ -8,15 +8,21 @@ namespace SpaceGame.Controllers
     {
         [SerializeField] private Vector3 direction;
         [Range(0f, 1f)] [SerializeField] private float factor;
+        [SerializeField] private float speed = 1f;
+        
         private Vector3 startPosition;
-
+        private const float FULL_CIRCLE = Mathf.PI * 2f;
         private void Awake()
         {
             startPosition = transform.position;
         }
-
         private void Update()
         {
+            float cycle = Time.time / speed;
+            float sinWave = Mathf.Sin(cycle * FULL_CIRCLE);
+            
+            factor = Mathf.Abs(sinWave);
+
             Vector3 offset = direction * factor;
             transform.position = offset + startPosition;
         }
